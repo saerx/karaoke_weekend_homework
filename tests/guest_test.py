c9d87e83c2c97d0eed5537c841540c6bc2065103
saerx <saerlaith.robyn@gmail.com>
2020-11-13 15:04:36 +0000

    set up TestGuest

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,4 +4,8 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.Guest1 
+        self.guest_1 = Guest(Lovefoxxx, 36, 50, "Let's Make Love and Listen to Death from Above")
+        self.guest_2 = Guest(Britney Spears, 38, 100, "Club Tropicana")
+        self.guest_3 = Guest(Damon Albarn, 52, 20, "E-Mail My Heart")
+        self.guest_4 = Guest(Jamiroquai, 50, 5, "Virtual Insanity")
+        self.guest_5 = Guest(Human Child, 8, 20, "Song 2")
