372bc9105663b69c62886713cdbd22c2f76233b3
saerx <saerlaith.robyn@gmail.com>
2020-11-13 18:58:27 +0000

    added drink class and tested

--- tests/drink_test.py
+++ tests/drink_test.py
@@ -1,17 +1,17 @@
-# import unittest
-# from classes.drink import Drink
+import unittest
+from classes.drink import Drink
 
 
-# class TestDrink(unittest.TestCase):
-#     def setUp(self):
-#         self.drink = Drink("Guiness", 3, 2)
+class TestDrink(unittest.TestCase):
+    def setUp(self):
+        self.drink = Drink("Guiness", 3, 2)
 
     
-#     def test_drink_has_name(self):
-#         self.assertEqual("Guiness", self.drink.name)
+    def test_drink_has_name(self):
+        self.assertEqual("Guiness", self.drink.name)
 
-#     def test_drink_has_price(self):
-#         self.assertEqual(3, self.drink.price)
+    def test_drink_has_price(self):
+        self.assertEqual(3, self.drink.price)
 
-#     def test_drink_has_alcohol_level(self):
-#         self.assertEqual(2, self.drink.alcohol_level)
+    def test_drink_has_alcohol_level(self):
+        self.assertEqual(2, self.drink.alcohol_level)
