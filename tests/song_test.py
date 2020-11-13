f9e15bde84d2d33ec382e4d155efa36c5dc4c015
saerx <saerlaith.robyn@gmail.com>
2020-11-13 14:35:38 +0000

    tested song.code_num

--- tests/song_test.py
+++ tests/song_test.py
@@ -4,6 +4,11 @@ from classes.song import Song
 
 class TestSong(unittest.TestCase):
     def setUp(self):
-        self.song_1 = Song(10001, "Club Tropicana", "Wham!", [4, 28], 1983)
-        self.song_2 = Song(10002, "Let's Make Love and Listen to Death from Above", "CSS", [3,30], 2005)
+        self.song_1 = Song(10001, "Let's Make Love and Listen to Death from Above", "CSS", [3,30], 2005)
+        self.song_2 = Song(10002, "Song 2", "Blur", [2, 2], 1996)
         self.song_3 = Song(10003, "E-Mail My Heart", "Britney Spears", [3, 41], 1999)
+        self.song_4 = Song(10004, "Club Tropicana", "Wham!", [4, 28], 1983)
+
+
+    def test_song_has_code_num(self):
+        self.assertEqual(10001, self.song_1.code_num)
