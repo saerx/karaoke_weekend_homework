64033e64862abaedc1b7457192a52b3c804cac2b
saerx <saerlaith.robyn@gmail.com>
2020-11-13 14:57:21 +0000

    added Guest and TestGuest class

---
+++ classes/guest.py
@@ -0,0 +1,7 @@
+class Guest:
+    def __init__(self, name, age, wallet, favourite_song):
+        self.name = name
+        self.age = age
+        self.wallet = wallet
+        self.favourite_song = favourite_song
+        self.drinks = []
---
+++ tests/guest_test.py
@@ -0,0 +1,7 @@
+import unittest
+
+from classes.guest import Guest 
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.Guest1 
